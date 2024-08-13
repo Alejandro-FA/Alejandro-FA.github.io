7c3c82aa13afbb77adeacec66aeb2ab3026a66a6
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2024-08-13 20:15:18 +0200

    Solve build error

--- app/lib/utils.dart
+++ app/lib/utils.dart
@@ -6,8 +6,12 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 /// To get an accurate width, the [textStyle] should be the same as the style used
 /// to render the text on the screen, as well as the values of [maxlines] and
 /// [textDirection].
-double computeTextWidth(String text,
-    {TextStyle? textStyle, int maxlines = 1, TextDirection textDirection = TextDirection.ltr}) {
+double computeTextWidth(
+  String text, {
+  TextStyle? textStyle,
+  int maxlines = 1,
+  TextDirection textDirection = TextDirection.ltr,
+}) {
   final TextPainter textPainter = TextPainter(
     text: TextSpan(text: text, style: textStyle),
     maxLines: maxlines,
@@ -22,9 +26,8 @@ double computeTextWidth(String text,
 /// For GitHub pages, it is set to [HashUrlStrategy], as it does not support
 /// path-based URLs. For all other platforms, it is set to [PathUrlStrategy].
 void decidetUrlStrategy() {
-  if (Uri.base.host.endsWith('github.io')) {
-    setUrlStrategy(const HashUrlStrategy());
-  } else {
-    setUrlStrategy(const PathUrlStrategy());
-  }
+  final isGitHub = Uri.base.host.endsWith('github.io');
+  // ignore: prefer_const_constructors
+  final strategy = isGitHub ? HashUrlStrategy() : PathUrlStrategy();
+  setUrlStrategy(strategy);
 }
